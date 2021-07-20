3426aa7fe1c968b2cf08e182206529b3919e6f38
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-07-20 19:59:23 +0800

    Create Trapper.cs

---
+++ ConcentratedHell/Guns/Trapper.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConcentratedHell
+{
+    class Trapper : Gun
+    {
+        public Trapper()
+        {
+            AmmoUsage = 0;
+
+            Type = GunType.Trapper;
+            AmmoType = Projectile.ProjectileType.GravTrap;
+            Sprite = GunSprites[Type];
+
+            Main.PlayerUpdateEvent += Update;
+            Rendering.DrawPlayer += Draw;
+            DestroyEvent += Destroy;
+
+            Cooldown = new GameValue("Cooldown", 0, 100, 5, 0);
+
+            FiringEvent += Fire;
+        }
+
+        void Fire()
+        {
+            var x = new GravTrap(MathHelper.ToDegrees(Player.Instance.RadiansToMouse), Position, 10f);
+        }
+    }
+}
