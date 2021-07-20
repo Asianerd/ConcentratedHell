29882cac9245018a1ee1e59e2c38bad5457c004c
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-07-20 19:59:15 +0800

    Create MissileLauncher.cs

---
+++ ConcentratedHell/Guns/MissileLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConcentratedHell
+{
+    class MissileLauncher : Gun
+    {
+        public MissileLauncher()
+        {
+            Type = GunType.MissileLauncher;
+            AmmoType = Projectile.ProjectileType.SeekingMissile;
+            Sprite = GunSprites[Type];
+
+            Main.PlayerUpdateEvent += Update;
+            Rendering.DrawPlayer += Draw;
+            DestroyEvent += Destroy;
+
+            Cooldown = new GameValue("Cooldown", 0, 100, 5, 0);
+
+            FiringEvent += Fire;
+        }
+
+        void Fire()
+        {
+            var x = new SeekingMissile(MathHelper.ToDegrees(Player.Instance.RadiansToMouse), Position, 10f);
+        }
+    }
+}
