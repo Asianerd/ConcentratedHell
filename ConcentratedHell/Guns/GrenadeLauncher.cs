54abda633e29541790ff272050aedef3b135831f
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-07-20 19:59:12 +0800

    Create GrenadeLauncher.cs

---
+++ ConcentratedHell/Guns/GrenadeLauncher.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace ConcentratedHell
+{
+    class GrenadeLauncher : Gun
+    {
+        public GrenadeLauncher()
+        {
+            Type = GunType.GrenadeLauncher;
+            AmmoType = Projectile.ProjectileType.Grenade;
+            Sprite = GunSprites[Type];
+
+            Main.PlayerUpdateEvent += Update;
+            Rendering.DrawPlayer += Draw;
+            DestroyEvent += Destroy;
+
+            Cooldown = new GameValue("Cooldown", 0, 100, 5, 0);
+
+            FiringEvent += Fire;
+        }
+
+        void Fire()
+        {
+            var x = new Grenade(MathHelper.ToDegrees(Player.Instance.RadiansToMouse), Position, 10f);
+        }
+    }
+}
