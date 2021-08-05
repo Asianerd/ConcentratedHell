0422706477b14ae5eeab403258cca77361a5d45d
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-08-05 14:16:13 +0800

    More methods for convenience

--- ConcentratedHell/GameValue.cs
+++ ConcentratedHell/GameValue.cs
@@ -15,7 +15,7 @@ namespace ConcentratedHell
         // Min                  Max  Min                  Max  Min                  Max
 
 
-        public GameValue(string _name, double _min, double _max, double _regeneration, double _iPercent = 100.0) // i is some adsurbly random number i wont ever use in initialization
+        public GameValue(string _name, double _min, double _max, double _regeneration, double _iPercent = 100.0)
         {
             Name = _name;
             Min = _min;
@@ -47,6 +47,33 @@ namespace ConcentratedHell
         public double Percent()
         {
             return I / (Max - Min);
+        }
+
+        public double PercentToValue(float _percent)
+        {
+            return (Max - Min) * _percent;
+        }
+
+
+        public void AffectMax(double _amount)
+        {
+            Max += _amount;
+        }
+
+        public void AffectMax(float _percent)
+        {
+            Max *= _percent; // Percent = 0f to 1f
+        }
+
+
+        public void AffectMin(double _amount)
+        {
+            Min += _amount;
+        }
+
+        public void AffectMin(float _percent)
+        {
+            Min *= _percent; // Percent = 0f to 1f
         }
     }
 }
