400734318e56cca1fe4f8622fea5affbe3d99c1d
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-08-05 14:18:41 +0800

    GREATEST COMMIT - CAMERA INTERGRATION

--- ConcentratedHell/Rendering.cs
+++ ConcentratedHell/Rendering.cs
@@ -14,61 +14,81 @@ namespace ConcentratedHell
         public static event Render DrawCursor;
         public static event Render DrawItems;
 
-        public static SpriteBatch PlayerSpriteBatch;
-        public static SpriteBatch EntitySpriteBatch;
-        public static SpriteBatch CursorSpriteBatch;
-        public static SpriteBatch ItemSpriteBatch;
+        public static SpriteBatch MainSpriteBatch;
 
         public static int SizingScale = 64;
         /* So that sprites are rendered to the proper size */
+        public static int FontWidth = 12;
+        /* Font width in pixels */
         public static Vector3 ScreenOffset = Vector3.Zero;
         public static GameValue ScreenShakeValue;
         public static int ScreenShakeIntensity;
 
+        #region Attributes
+        public static Texture2D Background;
+        public static Vector2 BackgroundOrigin;
+        public static Texture2D BackgroundWalls;
+        public static Vector2 BackgroundWallsOrigin;
+        public static Vector3 CameraPosition;
+        #endregion
+
         #region Rendering
-        public static void Initialize(SpriteBatch _playerSpriteBatch, SpriteBatch _entitySpriteBatch, SpriteBatch _cursorSpriteBatch, SpriteBatch _itemSpriteBatch)
+        public static void Initialize(SpriteBatch _mainSpriteBatch, Texture2D _background, Texture2D _backgroundWalls)
         {
             Main.PlayerUpdateEvent += Update;
 
-            PlayerSpriteBatch = _playerSpriteBatch;
-            EntitySpriteBatch = _entitySpriteBatch;
-            CursorSpriteBatch = _cursorSpriteBatch;
-            ItemSpriteBatch = _itemSpriteBatch;
+            MainSpriteBatch = _mainSpriteBatch;
+            Background = _background;
+            BackgroundWalls = _backgroundWalls;
+
+            BackgroundOrigin = new Vector2(960, 540);
+            BackgroundWallsOrigin = new Vector2(1048, 627);
 
             ScreenShakeValue = new GameValue("Screenshake", 0, 25, 1);
         }
 
         public static void RenderObjects()
         {
-            Matrix _renderedOffset = Matrix.CreateTranslation(ScreenOffset);
-            PlayerSpriteBatch.Begin(transformMatrix: _renderedOffset);
-            EntitySpriteBatch.Begin(transformMatrix: _renderedOffset);
-            ItemSpriteBatch.Begin(transformMatrix: _renderedOffset);
+            /* Drawing order:
+             * 
+             * Background
+             * Entities
+             * Items
+             * Player
+             * UI
+             * Cursor
+             */
+            CameraPosition = new Vector3((Main.screenSize / 2) - Player.Instance.Position, 0f) + ScreenOffset;
+            //_cameraPosition = new Vector3(Math.Clamp(_cameraPosition.X, -1920, 1920f), Math.Clamp(_cameraPosition.Y, -1080, 1080), 0f);
+            Matrix _renderedOffset = Matrix.CreateTranslation(CameraPosition);
+            MainSpriteBatch.Begin(transformMatrix:_renderedOffset); // Drawn in real world
+            MainSpriteBatch.Draw(Background, Vector2.Zero, null, Color.White, 0f, BackgroundOrigin, 2f, SpriteEffects.None, 0f);
+            MainSpriteBatch.Draw(BackgroundWalls, Vector2.Zero, null, Color.White, 0f, BackgroundWallsOrigin, 2f, SpriteEffects.None, 0f);
             if (DrawEntities != null)
             {
-                DrawEntities(PlayerSpriteBatch);
-            }
-            if (DrawItems != null)
-            {
-                DrawItems(ItemSpriteBatch);
+                DrawEntities(MainSpriteBatch);
             }
             if (DrawPlayer != null)
             {
-                DrawPlayer(EntitySpriteBatch);
+                DrawPlayer(MainSpriteBatch);
             }
-            ItemSpriteBatch.End();
-            PlayerSpriteBatch.End();
-            EntitySpriteBatch.End();
+            if (DrawItems != null)
+            {
+                DrawItems(MainSpriteBatch);
+            }
+            MainSpriteBatch.End();
+            MainSpriteBatch.Begin(); // Drawn in camera world
+            UI.Instance.Draw(MainSpriteBatch);
+            RenderCursor();
+            MainSpriteBatch.End();
         }
 
         public static void RenderCursor()
         {
-            CursorSpriteBatch.Begin();
-            if (CursorSpriteBatch != null)
+            if (DrawCursor != null)
             {
-                DrawCursor(CursorSpriteBatch);
+                DrawCursor(MainSpriteBatch);
             }
-            CursorSpriteBatch.End();
         }
         #endregion
 
