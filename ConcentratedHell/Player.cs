099243239d46ef0967a930f573433152ada9952d
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-08-05 14:17:31 +0800

    Combo system

--- ConcentratedHell/Player.cs
+++ ConcentratedHell/Player.cs
@@ -14,7 +14,7 @@ namespace ConcentratedHell
 
         public static void Initialize(Texture2D _bodySprite, List<Texture2D> _eyeSprites)
         {
-            Instance = new Player(Main.screenSize / 2);
+            Instance = new Player(new Vector2(0, 0));
             Instance.__privateInit__(_bodySprite, _eyeSprites);
         }
 
@@ -31,12 +31,31 @@ namespace ConcentratedHell
             #region Game attributes
             GunEquipped = Gun.InstantiateGun(Gun.GunType.Shotgun, out GunEquppedObject);
             Health = new GameValue("Health", 0, 100, 5);
-            Stamina = new GameValue("Stamina", 0, 500, 2);
+            Stamina = new GameValue("Stamina", 0, 50000, 2);
+            Combo = new GameValue("Combo", 0, 100, -0.1);
+            Combo.I = 0;
+            Level = 0;
             foreach(Projectile.ProjectileType x in Enum.GetValues(typeof(Projectile.ProjectileType)))
             {
-                AmmoInventory[x] = 5;
+                AmmoInventory[x] = 500;
             }
             #endregion
+
+            /*for (int _x = -1900; _x < 1900; _x += 64)
+            {
+                for (int _y = -1000; _y < 1000; _y += 64)
+                {
+                    var _enemy = new Enemy(new Vector2(_x, _y));
+                }
+            }*/
+            /*for (int _x = 0; _x < 10000; _x++)
+            {
+                var _enemy = new Enemy(new Vector2(500, 500));
+            }*/
+            /*for (int _x = 0; _x < 5000; _x++)
+            {
+                var _item = new Item(new Vector2(500, 500), Item.ItemClass.Ammo, (Projectile.ProjectileType)Universe.RANDOM.Next(0, 7));
+            }*/
         }
         #endregion
 
@@ -64,7 +83,10 @@ namespace ConcentratedHell
         public Gun GunEquppedObject;
         public GameValue Health;
         public GameValue Stamina;
+        public GameValue Combo;
+        public int Level;
         public bool TimeStopped = false;
+        public bool CanFire = false;
         public Dictionary<Projectile.ProjectileType, int> AmmoInventory = new Dictionary<Projectile.ProjectileType, int>();
         #endregion
         #endregion
@@ -76,9 +98,10 @@ namespace ConcentratedHell
         
         public void Update()
         {
-            MousePosition = Mouse.GetState().Position.ToVector2();
+            MousePosition = Position - ((Main.screenSize / 2) - Mouse.GetState().Position.ToVector2());
             RadiansToMouse = Universe.ANGLETO(Position, MousePosition, false);
             Interact();
+            CheckCombo();
             Move();
             Eyes.Update();
         }
@@ -90,7 +113,7 @@ namespace ConcentratedHell
 
             float _speed = _sprinting && (Stamina.I > 0) ? SprintMultiplier * Speed : Speed;
 
-            #region Weapon changing
+            /*#region Weapon changing
             if(_kInput.IsKeyDown(Keys.D1))
             {
                 GunEquipped = Gun.InstantiateGun(Gun.GunType.Glock, out GunEquppedObject);
@@ -119,9 +142,9 @@ namespace ConcentratedHell
             {
                 GunEquipped = Gun.InstantiateGun(Gun.GunType.Trapper, out GunEquppedObject);
             }
-            #endregion
+            #endregion*/
 
-
+            CanFire = !_kInput.IsKeyDown(Keys.Tab);
 
             #region Movement
             Vector2 _startingPosition = Position;
@@ -147,6 +170,7 @@ namespace ConcentratedHell
                 MovingDirection.Normalize();
             }
             Position += MovingDirection * _speed;
+            Position = new Vector2(Math.Clamp(Position.X, -Main.BoundingBox.X+32, Main.BoundingBox.X-32), Math.Clamp(Position.Y, -Main.BoundingBox.Y+32, Main.BoundingBox.Y-32));
 
             Vector2 _endingPosition = Position;
             #endregion
@@ -162,6 +186,7 @@ namespace ConcentratedHell
             }
 
             Health.Regenerate();
+            Combo.AffectValue(-0.5d);
             #endregion
         }
 
@@ -172,11 +197,18 @@ namespace ConcentratedHell
 
             if(_mInput.RightButton == ButtonState.Pressed)
             {
-                //var x = new Enemy(new Vector2(Universe.RANDOM.Next(0, (int)Main.screenSize.X), Universe.RANDOM.Next(0, (int)Main.screenSize.Y)));
                 var x = new Enemy(MousePosition);
             }
 
             TimeStopped = _kInput.IsKeyDown(Keys.Space);
+
+            if (_kInput.IsKeyDown(Keys.F))
+            {
+                for (int _n = 0; _n < 50; _n++)
+                {
+                    var _item = new Item(Position, Item.ItemClass.Ammo, (Projectile.ProjectileType)Universe.RANDOM.Next(0, 7));
+                }
+            }
         }
 
         public void Draw(SpriteBatch spriteBatch)
@@ -194,6 +226,23 @@ namespace ConcentratedHell
                 0f
                 );
             Eyes.Draw(spriteBatch, Position, Size);
+        }
+
+        public void CheckCombo()
+        {
+            if (Combo.Percent() >= 1)
+            {
+                Combo.AffectValue(0.2f);
+                Level++;
+            }
+            else
+            {
+                if (Combo.Percent() <= 0 && Level > 0)
+                {
+                    Combo.AffectValue(0.9f);
+                    Level--;
+                }
+            }
         }
     }
 }
