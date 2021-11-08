841be06d4be200027fc57c664481146b7a01048f
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-11-08 20:19:06 +0800

    Cache bs and some stuff

---
+++ ConcentratedHell/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell
+{
+    class Player
+    {
+        public static Player Instance = null;
+        public static Texture2D sprite;
+        
+        public static void Initialize()
+        {
+            if(Instance == null)
+            {
+                Instance = new Player();
+            }
+        }
+
+        public static void LoadContent(Texture2D _sprite)
+        {
+            sprite = _sprite;
+        }
+
+        public Vector2 position;
+
+        public void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch spritebatch)
+        {
+            spritebatch.Draw(sprite, position, null, Color.White, 0f, sprite.Bounds.Center.ToVector2(), 1f, SpriteEffects.None, 0f);
+        }
+    }
+}
