ac99808ea84ac1654a01fe2226dbfc9f4f14a3a5
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-12-30 20:25:43 +0800

    Camera intergration (lerping action)

--- ConcentratedHell/Main.cs
+++ ConcentratedHell/Main.cs
@@ -1,23 +1,67 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
 
 namespace ConcentratedHell
 {
     public class Main : Game
     {
+        public static Rectangle screenSize = new Rectangle(0, 0, 1920, 1080);
+        //public static Rectangle screenSize = new Rectangle(0, 0, 800, 800);
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+
+        public delegate void RenderEvent(SpriteBatch spriteBatch);
+        public static RenderEvent DrawEvent;
+
+        public delegate void GameEvent();
+        public static GameEvent UpdateEvent;
+
+        public static KeyboardState keyboardState;
+        public static MouseState mouseState;
 
         public Main()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = screenSize.Width;
+            _graphics.PreferredBackBufferHeight = screenSize.Height;
+            if(screenSize == new Rectangle(0, 0, 1920, 1080))
+            {
+                _graphics.IsFullScreen = true;
+                _graphics.ApplyChanges();
+            }
+
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
 
         protected override void Initialize()
         {
+            Map.placeholderSprite = Content.Load<Texture2D>("blank");
+            Map.Initialize(new List<Tile>(){
+                new Tile(new Rectangle(-128, -128, 1152, 128)),
+                new Tile(new Rectangle(-128, -128, 128, 896)),
+                new Tile(new Rectangle(896, -128, 128, 896)),
+                new Tile(new Rectangle(-128, 640, 1152, 128)),
+
+                new Tile(new Rectangle(128, 128, 128, 128)),
+                new Tile(new Rectangle(128, 384, 128, 128)),
+                new Tile(new Rectangle(640, 256, 128, 128)),
+
+                new Tile(new Rectangle(384, 0, 128, 256)),
+                new Tile(new Rectangle(384, 384, 128, 256)),
+            });
+            Input.Initialize(new List<Input>()
+            {
+                new Input(Keys.F11, () => {
+                    _graphics.IsFullScreen = !_graphics.IsFullScreen;
+                    _graphics.ApplyChanges();
+                })
+            });
+            Player.Initialize();
+            Camera.Initialize();
 
             base.Initialize();
         }
@@ -26,6 +70,7 @@ namespace ConcentratedHell
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            Player.LoadContent(Content.Load<Texture2D>("player"));
         }
 
         protected override void Update(GameTime gameTime)
@@ -33,12 +78,30 @@ namespace ConcentratedHell
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            mouseState = Mouse.GetState();
+            keyboardState = Keyboard.GetState();
+
+            Input.StaticUpdate();
+
+            if (UpdateEvent != null)
+            {
+                UpdateEvent();
+            }
+
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            Matrix renderPosition = Matrix.CreateTranslation(new Vector3((screenSize.Size.ToVector2() / 2f) - Camera.Instance.position, 0));
+            _spriteBatch.Begin(samplerState:SamplerState.PointClamp, transformMatrix:renderPosition);
+            if (DrawEvent != null)
+            {
+                DrawEvent(_spriteBatch);
+            }
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
