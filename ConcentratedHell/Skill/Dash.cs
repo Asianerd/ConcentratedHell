c297ffa4b8ff4be8fd9d4b2231920d4676c43690
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2021-12-31 16:41:30 +0800

    (semi)Completed dash class; Might add particles

--- ConcentratedHell/Skill/Dash.cs
+++ ConcentratedHell/Skill/Dash.cs
@@ -7,13 +7,66 @@ namespace ConcentratedHell
 {
     class Dash : Skill
     {
+        GameValue dashPower;
+        bool dashing = false;
+        float dashSpeed = 30f;
+
+        public Dash():base()
+        {
+            dashPower = new GameValue(0, 5, -1, 100);
+        }
+
         public override void Execute()
         {
-            Debug.WriteLine("Dash was executed");
+            if(!Player.Instance.hasMovementInput)
+            {
+                return;
+            }
             if (cooldown.Percent() >= 0.5f)
             {
-                Debug.WriteLine("Something");
                 cooldown.I -= cooldown.PercentToValue(0.5f);
+                dashing = true;
+            }
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            if (dashing)
+            {
+                ApplyDash();
+
+                dashPower.Regenerate();
+                if (dashPower.Percent() <= 0)
+                {
+                    dashing = false;
+                }
+            }
+            else
+            {
+                if (dashPower.Percent() < 1)
+                {
+                    dashPower.AffectValue(1d);
+                }
+            }
+        }
+
+        public void ApplyDash()
+        {
+            Point increment = new Point(
+                (int)(MathF.Cos(Player.Instance.direction) * dashSpeed),
+                (int)(MathF.Sin(Player.Instance.direction) * dashSpeed)
+                );
+
+            Rectangle target = new Rectangle(
+                Player.Instance.rect.Location + increment,
+                Player.Instance.rect.Size
+                );
+
+            if(Map.IsValidPosition(target))
+            {
+                Player.Instance.rect.Location = target.Location;
             }
         }
     }
