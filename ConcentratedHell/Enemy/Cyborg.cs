ff61e3aae72e1a6f8558ff62727da5e87ecd4d0f
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-01 03:40:04 +0800

    Cyborg class

---
+++ ConcentratedHell/Enemy/Cyborg.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell
+{
+    class Cyborg:Enemy
+    {
+        public Cyborg(Rectangle rect) : base(rect, Type.Cyborg)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+        }
+
+        public override void Draw()
+        {
+            base.Draw();
+
+        }
+    }
+}
