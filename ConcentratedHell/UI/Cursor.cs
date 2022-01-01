2e406a8389e4c3cfb9df83f19713c6de343e62d6
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-02 06:49:11 +0800

    Added Cursor and UI classes (+ mouse isn't visible now, only custom cursor)

---
+++ ConcentratedHell/UI/Cursor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell
+{
+    class Cursor
+    {
+        public static Cursor Instance;
+        public static Texture2D sprite;
+
+        public static void Initialize()
+        {
+            Instance = new Cursor();
+        }
+
+        public static void LoadContent(Texture2D _sprite)
+        {
+            sprite = _sprite;
+        }
+
+        public Vector2 screenPosition;
+        public Vector2 worldPosition;
+        public float playerToCursor;
+
+        public void Update()
+        {
+            screenPosition = Main.mouseState.Position.ToVector2();
+            worldPosition = Camera.ScreenToWorld(screenPosition);
+            playerToCursor = MathF.Atan2(
+                worldPosition.Y - Player.Instance.rect.Center.Y,
+                worldPosition.X - Player.Instance.rect.Center.X
+                );
+        }
+
+        public void Draw()
+        {
+            Main.spriteBatch.Draw(sprite, screenPosition, Color.White);
+        }
+    }
+}
---
+++ ConcentratedHell/UI/UI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell
+{
+    class UI
+    {
+        public static UI Instance;
+        public static SpriteFont font;
+
+        public static void Initialize()
+        {
+            Instance = new UI();
+            Cursor.Initialize();
+
+            Main.UpdateEvent += Update;
+        }
+
+        public static void LoadContent(SpriteFont _font)
+        {
+            font = _font;
+        }
+
+        public static void Update()
+        {
+            Cursor.Instance.Update();
+        }
+
+        public static void Draw()
+        {
+            Cursor.Instance.Draw();
+        }
+    }
+}
