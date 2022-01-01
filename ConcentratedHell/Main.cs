f8b397532d07b86b55fd94795bf3fde55072742c
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-02 06:48:01 +0800

    Added singleton-ish thing for ease

--- ConcentratedHell/Main.cs
+++ ConcentratedHell/Main.cs
@@ -3,11 +3,14 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System;
+using ConcentratedHell.Combat;
 
 namespace ConcentratedHell
 {
     public class Main : Game
     {
+        public static Main Instance;
+
         public static Rectangle screenSize = new Rectangle(0, 0, 1000, 1000);
         public static SpriteFont mainFont;
         public static GraphicsDeviceManager graphics;
@@ -24,21 +27,40 @@ namespace ConcentratedHell
 
         public Main()
         {
+            Instance = this;
+
             graphics = new GraphicsDeviceManager(this);
             graphics.PreferredBackBufferWidth = screenSize.Width;
             graphics.PreferredBackBufferHeight = screenSize.Height;
             if(screenSize == new Rectangle(0, 0, 1920, 1080))
             {
                 graphics.IsFullScreen = true;
-                graphics.ApplyChanges();
             }
+            graphics.ApplyChanges();
 
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            IsMouseVisible = false;
         }
 
         protected override void Initialize()
         {
+            UI.Initialize();
+
+            Input.Initialize(new Dictionary<Keys, Input>()
+            {
+                { Keys.F11,
+                    new Input(Keys.F11, () => {
+                        graphics.IsFullScreen = !graphics.IsFullScreen;
+                        graphics.ApplyChanges();
+                    })
+                },
+                { Keys.LeftShift, new Input(Keys.LeftShift) },
+                { Keys.B, new Input(Keys.B, () => {
+                        var x = new Cyborg(Player.Instance.rect);
+                    })
+                }
+            });
+
             Map.placeholderSprite = Content.Load<Texture2D>("blank");
             Map.Initialize(new List<Tile>(){
                 // Doors first to avoid weird graphic inconsistencies
@@ -56,25 +78,14 @@ namespace ConcentratedHell
                 new Tile(new Rectangle(384, 0, 128, 256)),
                 new Tile(new Rectangle(384, 384, 128, 256)),
             });
-            Input.Initialize(new Dictionary<Keys, Input>()
-            {
-                { Keys.F11,
-                    new Input(Keys.F11, () => {
-                        graphics.IsFullScreen = !graphics.IsFullScreen;
-                        graphics.ApplyChanges();
-                    })
-                },
-                { Keys.LeftShift, new Input(Keys.LeftShift) },
-                { Keys.B, new Input(Keys.B, () => {
-                        var x = new Cyborg(Player.Instance.rect);
-                    })
-                },
-            });
             Player.Initialize();
             Camera.Initialize();
 
             Skill.Initialize();
             Enemy.Initialize();
+
+            Combat.Projectiles.Projectile.LoadContent();
+            Combat.Projectiles.Projectile.Initialize();
 
             base.Initialize();
         }
@@ -83,13 +94,12 @@ namespace ConcentratedHell
         {
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            Cursor.LoadContent(Content.Load<Texture2D>("cursor"));
+            UI.LoadContent(Content.Load<SpriteFont>("Fonts/mainFont"));
+
             mainFont = Content.Load<SpriteFont>("Fonts/MainFont");
             Player.LoadContent(Content.Load<Texture2D>("player"));
-            string _enemySpritePath = "Enemy";
-            Enemy.LoadContent(new Dictionary<Enemy.Type, Texture2D>()
-            {
-                { Enemy.Type.Cyborg, Content.Load<Texture2D>($"{_enemySpritePath}/Cyborg") },
-            });
+            Enemy.LoadContent(Content.Load<Texture2D>("Enemy/hotbar"));
         }
 
         protected override void Update(GameTime gameTime)
@@ -114,12 +124,16 @@ namespace ConcentratedHell
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            Matrix renderPosition = Matrix.CreateTranslation(new Vector3((screenSize.Size.ToVector2() / 2f) - Camera.Instance.position, 0));
+            Matrix renderPosition = Matrix.CreateTranslation(new Vector3(Camera.Instance.offset, 0));
             spriteBatch.Begin(samplerState:SamplerState.PointClamp, transformMatrix:renderPosition);
             if (DrawEvent != null)
             {
                 DrawEvent();
             }
+            spriteBatch.End();
+
+            spriteBatch.Begin();
+            UI.Draw();
             spriteBatch.End();
 
             base.Draw(gameTime);
