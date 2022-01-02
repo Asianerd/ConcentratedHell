fc5116f980656c4e48379d2615c72995f870e6be
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-02 11:30:06 +0800

    Weapon-related stuff (equipping and picking up)

--- ConcentratedHell/Player.cs
+++ ConcentratedHell/Player.cs
@@ -5,11 +5,13 @@ using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ConcentratedHell.Combat;
 
 namespace ConcentratedHell
 {
     class Player
     {
+        #region Static
         public static Player Instance = null;
         public static Texture2D sprite;
         
@@ -28,44 +30,57 @@ namespace ConcentratedHell
         {
             sprite = _sprite;
         }
+        #endregion
 
         public Rectangle rect;
         public static Vector2 size;
         public float direction = 0f; // Radians
         public float speed = 5;
-        public bool stuck = false;
         public bool hasMovementInput = false;
 
         public Dictionary<Ammo.Type, int> ammoInventory;
+        public Dictionary<Weapon.Type, Weapon> arsenal;
+        public Weapon equippedWeapon = null;
 
         Player()
         {
             rect = new Rectangle(0, 0, 64, 64);
             size = new Vector2(64, 64);
+
+            ammoInventory = new Dictionary<Ammo.Type, int>();
+            foreach (Ammo.Type x in Enum.GetValues(typeof(Ammo.Type)).Cast<Ammo.Type>())
+            {
+                ammoInventory.Add(x, 100);
+            }
+
+            arsenal = new Dictionary<Weapon.Type, Weapon>();
         }
 
         public void Update()
         {
-            speed = 8f * (Main.keyboardState.IsKeyDown(Keys.LeftControl) ? 0.5f : 1f);
+            speed = 8f * (Main.keyboardState.IsKeyDown(Keys.LeftControl) ? 0.5f : 1f) * Universe.speedMultiplier;
 
-            if (MouseInput.LMouse.active)
+            if (!UI.UI.selectionActive)
             {
-                var x = new Combat.Projectiles.Generic(rect.Center.ToVector2(), Cursor.Instance.playerToCursor);
+                if (equippedWeapon != null)
+                {
+                    equippedWeapon.Update();
+                }
             }
+
             if (MouseInput.RMouse.active)
             {
                 var x = new Cyborg(new Rectangle(Cursor.Instance.worldPosition.ToPoint(), rect.Size));
             }
 
-            ImprovedPlayerMovement();
+            Move();
             if (Input.inputs[Keys.LeftShift].active)
             {
                 Skill.ExecuteSkill(Skill.Type.Dash);
             }
-            StuckCheck();
         }
 
-        public void ImprovedPlayerMovement()
+        public void Move()
         {
             Vector2 targetVelocity = Vector2.Zero;
             foreach (Keys key in Input.playerInputKeys)
@@ -110,11 +125,6 @@ namespace ConcentratedHell
             }
         }
 
-        public void StuckCheck()
-        {
-
-        }
-
         void CalculateDirection(Vector2 start, Vector2 end)
         {
             direction = MathF.Atan2(end.Y - start.Y, end.X - start.X);
@@ -123,6 +133,31 @@ namespace ConcentratedHell
         public void Draw()
         {
             Main.spriteBatch.Draw(sprite, rect, Color.White);
+            if(equippedWeapon != null)
+            {
+                Vector2 renderedPosition = new Vector2(
+
+                    );
+                Main.spriteBatch.Draw(equippedWeapon.sprite, rect.Location.ToVector2(), Color.White);
+            }
         }
+
+        #region Weapon-related
+        public void AddWeapon(Weapon weapon)
+        {
+            if (!arsenal.Keys.Contains(weapon.type))
+            {
+                arsenal.Add(weapon.type, weapon);
+                equippedWeapon = arsenal[weapon.type];
+
+                UI.SelectionWheel.SelectionWheel.Instance.UpdateSections();
+            }
+        }
+
+        public void EquipWeapon(Weapon.Type type)
+        {
+            equippedWeapon = arsenal[type];
+        }
+        #endregion
     }
 }
