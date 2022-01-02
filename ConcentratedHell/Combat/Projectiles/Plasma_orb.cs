9a224b5b06adf9a5590b79ff0ba88bf1761210f8
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-02 11:27:37 +0800

    Plasma rifle

---
+++ ConcentratedHell/Combat/Projectiles/Plasma_orb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ConcentratedHell.Combat.Projectiles
+{
+    class Plasma_orb:Projectile
+    {
+        public Plasma_orb(Vector2 position, float direction) : base(Type.Plasma_orb, 2f, 20f, position, direction)
+        {
+
+        }
+
+        public override void Update()
+        {
+            base.Update();
+            rotation += 0.1f * Universe.speedMultiplier;
+        }
+    }
+}
---
+++ ConcentratedHell/Combat/Weapons/Plasma_rifle.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcentratedHell.Combat.Weapons
+{
+    class Plasma_rifle:Weapon
+    {
+        public Plasma_rifle() : base(Type.Plasma_Rifle, Projectiles.Projectile.Type.Plasma_orb)
+        {
+
+        }
+
+        public override void Fire(Vector2 origin)
+        {
+            base.Fire(origin);
+            var x = new Projectiles.Plasma_orb(origin, Cursor.Instance.playerToCursor);
+        }
+    }
+}
