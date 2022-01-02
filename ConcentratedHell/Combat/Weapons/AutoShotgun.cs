bf722ef7da8ddd9efa1ce6d0f9b15b28063fddaf
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-02 19:07:50 +0800

    Auto-Shotgun class

---
+++ ConcentratedHell/Combat/Weapons/AutoShotgun.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcentratedHell.Combat.Weapons
+{
+    class AutoShotgun:Weapon
+    {
+        public AutoShotgun() : base(Type.Auto_Shotgun, Projectiles.Projectile.Type.Pellet, 65f, 1f, Ammo.Type.Shell, new GameValue(0, 15, 1))
+        {
+
+        }
+
+        public override void Fire(Vector2 origin)
+        {
+            base.Fire(origin);
+
+            for(int i = 0; i <= 30; i++)
+            {
+                var x = new Projectiles.Pellet(origin, Cursor.Instance.playerToCursor + Main.random.Next(-150, 150)/1000f, Main.random.Next(-300, 300) / 100f);
+            }
+        }
+    }
+}
