a340ab299350c06aa441f491ca9283f1ee56a764
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-03 09:18:39 +0800

    Ammo asset fetching

--- ConcentratedHell/Combat/Ammo.cs
+++ ConcentratedHell/Combat/Ammo.cs
@@ -1,11 +1,26 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace ConcentratedHell
 {
     class Ammo
     {
+        public static Dictionary<Type, Texture2D> spriteTable;
+
+        public static void LoadContent()
+        {
+            string ammoAssetPath = "Pickups/Ammo";
+            spriteTable = new Dictionary<Type, Texture2D>();
+            foreach(Type x in Enum.GetValues(typeof(Type)).Cast<Type>())
+            {
+                spriteTable.Add(x, Main.Instance.Content.Load<Texture2D>($"{ammoAssetPath}/{x.ToString().ToLower()}"));
+            }
+        }
+
         public enum Type
         {
             Small,
