dd0c499c45c89b19b66db336972bf1b672d2fd1c
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-03 09:18:57 +0800

    amongus guns :)

---
+++ ConcentratedHell/Combat/Projectiles/Amongus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell.Combat.Projectiles
+{
+    /*
+    class Amongus:Projectile
+    {
+        public Amongus(Vector2 position, float direction):base(Type.Amongus, 5f, 20f, position, direction)
+        {
+
+        }
+
+        public override void Draw()
+        {
+            Main.spriteBatch.Draw(sprite, position, null, Color.White, direction + rotation, spriteOrigin, 10f, Math.Abs(direction) >= (Math.PI / 2f) ? SpriteEffects.FlipVertically : SpriteEffects.None, 0f);
+        }
+    }*/
+}
---
+++ ConcentratedHell/Combat/Weapons/Amongus_Shotgun.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConcentratedHell.Combat.Weapons
+{
+    /*class Amongus_Shotgun:Weapon
+    {
+        public Amongus_Shotgun() : base(Type.Amongus_Shotgun, Projectiles.Projectile.Type.Amongus, 100f, 1f, Ammo.Type.Shell, new GameValue(0, 15, 1))
+        {
+
+        }
+
+        public override void Fire(Vector2 origin)
+        {
+            base.Fire(origin);
+
+            *//*for (int i = 0; i <= 5; i++)
+            {
+                var x = new Projectiles.Amongus(origin, );
+            }*//*
+            var x = new Projectiles.Amongus(origin, Cursor.Instance.playerToCursor);
+        }
+    }*/
+}
