98a7bcef09b4f1365f9e73d3eed7f7cd12fdd653
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-03 09:17:23 +0800

    Pickups

---
+++ ConcentratedHell/Pickups/AmmoPickup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell.Pickups
+{
+    class AmmoPickup:Pickup
+    {
+        public Ammo.Type type;
+
+        public AmmoPickup(Ammo.Type _type, int amount, Vector2 position) : base(PickupType.Ammo, amount, position, Ammo.spriteTable[_type])
+        {
+            type = _type;
+            renderedScale = 7f;
+            rotation = Main.random.Next(-300, 300) / 100f;
+        }
+
+        public override void Update()
+        {
+            base.Update();
+
+            rotation += 0.01f;
+        }
+
+        public override void AddToPlayer()
+        {
+            base.AddToPlayer();
+
+            Player.Instance.ammoInventory[type] += amount;
+        }
+    }
+}
---
+++ ConcentratedHell/Pickups/Pickup.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace ConcentratedHell.Pickups
+{
+    class Pickup
+    {
+        public static List<Pickup> pickups;
+
+        public static void Initialize()
+        {
+            pickups = new List<Pickup>();
+
+            Main.UpdateEvent += StaticUpdate;
+            Main.DrawEvent += StaticDraw;
+        }
+
+        public static void StaticUpdate()
+        {
+            foreach(Pickup x in pickups)
+            {
+                x.Update();
+            }
+            pickups.RemoveAll(n => !n.alive);
+        }
+
+        public static void StaticDraw()
+        {
+            foreach(Pickup x in pickups)
+            {
+                x.Draw();
+            }
+        }
+
+        #region Inherited
+        PickupType pickupType;
+        public int amount;
+        bool alive = true;
+
+        Vector2 position;
+
+        float detectRange = 100f;
+        float pickupRange = 20f;
+        bool detected = false;
+        GameValue progress;
+
+        public Texture2D sprite;
+        Vector2 spriteOrigin;
+        public float renderedScale = 1f;
+        public float rotation = 0f;
+
+        public Pickup(PickupType _type, int _amount, Vector2 _position, Texture2D _sprite)
+        {
+            pickupType = _type;
+            amount = _amount;
+
+            position = _position;
+            progress = new GameValue(0, 15, 1, 0);
+
+            sprite = _sprite;
+            spriteOrigin = sprite.Bounds.Center.ToVector2();
+
+            pickups.Add(this);
+        }
+
+        public virtual void Update()
+        {
+            if(!alive)
+            {
+                return;
+            }
+
+            float distance = Vector2.Distance(Player.Instance.rect.Center.ToVector2(), position);
+
+            if (!detected)
+            {
+                detected = distance <= detectRange;
+            }
+            else
+            {
+                progress.Regenerate();
+                position = Vector2.Lerp(position, Player.Instance.rect.Center.ToVector2(), (float)progress.Percent());
+
+                if(distance <= pickupRange)
+                {
+                    AddToPlayer();
+                    alive = false;
+                }
+            }
+        }
+
+        public virtual void AddToPlayer()
+        {
+
+        }
+
+        public virtual void Draw()
+        {
+            Main.spriteBatch.Draw(sprite, position, null, Color.White, rotation, spriteOrigin, renderedScale, SpriteEffects.None, 0f);
+        }
+        #endregion
+
+        public enum PickupType
+        {
+            Ammo,
+            Weapon,
+            Health,
+        }
+    }
+}
