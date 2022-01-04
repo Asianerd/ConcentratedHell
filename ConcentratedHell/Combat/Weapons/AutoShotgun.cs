3d2f02b7e707c08604e0c9b813158c895c68cacc
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-05 03:44:59 +0800

    Added Muzzle flash particles when firing

--- ConcentratedHell/Combat/Weapons/AutoShotgun.cs
+++ ConcentratedHell/Combat/Weapons/AutoShotgun.cs
@@ -7,7 +7,7 @@ namespace ConcentratedHell.Combat.Weapons
 {
     class AutoShotgun:Weapon
     {
-        public AutoShotgun() : base(Type.Auto_Shotgun, Projectiles.Projectile.Type.Pellet, 65f, 2f, Ammo.Type.Shell, new GameValue(0, 15, 1))
+        public AutoShotgun() : base(Type.Auto_Shotgun, Projectiles.Projectile.Type.Pellet, 85f, 2f, Ammo.Type.Shell, new GameValue(0, 15, 1))
         {
 
         }
@@ -19,6 +19,10 @@ namespace ConcentratedHell.Combat.Weapons
             for(int i = 0; i <= 30; i++)
             {
                 var x = new Projectiles.Pellet(origin, Cursor.Instance.playerToCursor + Main.random.Next(-150, 150) / 1000f, Main.random.Next(-300, 300) / 100f);
+            }
+            for (int i = 0; i <= 80; i++)
+            {
+                var y = new Particles.MuzzleFlashParticle(origin, Cursor.Instance.playerToCursor + (Main.random.Next(-800, 800) / 1000f), Main.random.Next(400, 1600) / 100f);
             }
         }
     }
