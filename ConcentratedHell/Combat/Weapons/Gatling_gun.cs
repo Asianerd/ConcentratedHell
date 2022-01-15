b7473bc3c9c79043c6a873fa94b612693fb7eb1c
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-16 04:13:15 +0800

    Gatling-gun has charge-up time

--- ConcentratedHell/Combat/Weapons/Gatling_gun.cs
+++ ConcentratedHell/Combat/Weapons/Gatling_gun.cs
@@ -8,16 +8,34 @@ namespace ConcentratedHell.Combat.Weapons
     class Gatling_gun:Weapon
     {
         int cartridgeCounter = 0;
+        GameValue progress;
 
-        public Gatling_gun():base(Type.Gatling_Gun, Projectiles.Projectile.Type.Small_round, 100f, 0.5f, Ammo.Type.Small, new GameValue(0, 1, 1), _ammoUsage:3)
+        float previous = 0;
+
+        public Gatling_gun():base(Type.Gatling_Gun, Projectiles.Projectile.Type.Small_round, 100f, 0.5f, Ammo.Type.Small, new GameValue(0, 2, 1), _ammoUsage:3)
         {
+            progress = new GameValue(0, 180, 1, 0);
+        }
 
+        public override void Update()
+        {
+            base.Update();
+
+            progress.Regenerate((MouseInput.LMouse.isPressed || MouseInput.RMouse.isPressed) ? 1 : -1);
+            speedMultiplier = 1f - (0.5f * (float)progress.Percent());
+            cooldown.Regeneration = progress.Percent();
+
+            if(Math.Abs(progress.I - previous) >= (5f * (1f-progress.Percent())))
+            {
+                previous = (float)progress.I;
+                AdvanceSprite();
+            }
         }
 
         public override void ExecuteFire()
         {
             Player.Instance.ammoInventory[ammoType] -= ammoUsage;
-            Vector2 origin = Vector2.Zero;
+            Vector2 origin;
             Vector2 mainOrigin = new Vector2(
                     (MathF.Cos(Cursor.Instance.playerToCursor) * projectileSpawnDistance) + Player.Instance.rect.Center.X,
                     (MathF.Sin(Cursor.Instance.playerToCursor) * projectileSpawnDistance) + Player.Instance.rect.Center.Y
