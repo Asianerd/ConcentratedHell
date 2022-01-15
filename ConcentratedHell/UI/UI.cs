84fcb90328d23350ad3c49b5b00e23390405b3f1
asian_nerd <61580137+Asianerd@users.noreply.github.com>
2022-01-16 04:11:20 +0800

    Added cooldown indicator

--- ConcentratedHell/UI/UI.cs
+++ ConcentratedHell/UI/UI.cs
@@ -16,9 +16,14 @@ namespace ConcentratedHell.UI
         public static bool selectionActive;
         public static Color errorColor;
 
+        public static Texture2D fireCooldownSprite;
+        public static Rectangle fireCooldownRect;
+
         public static Texture2D healthbarSprite;
         public static Texture2D healthbarCase;
         public static Rectangle healthbarRect;
+
+        public static Point ammoTextPositionOffset;
 
         public static bool showDebug = false;
         public static float fps;
@@ -31,9 +36,14 @@ namespace ConcentratedHell.UI
 
             errorColor = new Color(184, 30, 17);
 
+            fireCooldownSprite = Main.Instance.Content.Load<Texture2D>("UI/fire_cooldown_sprite");
+            fireCooldownRect = new Rectangle(0, 0, 35, 5);
+
             healthbarSprite = Main.Instance.Content.Load<Texture2D>("UI/Healthbar/healthbar");
             healthbarCase = Main.Instance.Content.Load<Texture2D>("UI/Healthbar/healthbar_case");
             healthbarRect = new Rectangle(0, 0, 780, 50);
+
+            ammoTextPositionOffset = new Point(300, 200);
 
             PickupText.Initialize();
 
@@ -62,8 +72,23 @@ namespace ConcentratedHell.UI
                 int ammoAmount = Player.Instance.ammoInventory[Player.Instance.equippedWeapon.ammoType];
                 string ammoText = ammoAmount > 0 ? ammoAmount.ToString() : "Out of ammo!";
                 Vector2 origin = font.MeasureString(ammoText) / 2f;
-                Vector2 renderedPosition = Cursor.Instance.screenPosition + new Vector2(2, 50);
-                Main.spriteBatch.DrawString(font, ammoText, renderedPosition, ammoAmount >= Player.Instance.equippedWeapon.ammoUsage ? Color.White : errorColor, 0f, origin, 1f, SpriteEffects.None, 0f);
+                Vector2 renderedPosition = Cursor.Instance.screenPosition + new Vector2(2, (Cursor.sprite.Bounds.Height*5f) + 15f);
+
+
+                Main.spriteBatch.DrawString(font, ammoText, renderedPosition, ammoAmount >= Player.Instance.equippedWeapon.ammoUsage ? Color.White : errorColor, 0f, origin, 0.8f, SpriteEffects.None, 0f);
+
+                int offset = (int)(Cursor.sprite.Bounds.Width * Player.Instance.equippedWeapon.cooldown.Percent() * 5f);
+                Main.spriteBatch.Draw(fireCooldownSprite, new Rectangle(
+                    new Point(
+                        (int)(Cursor.Instance.screenPosition.X - (Cursor.sprite.Bounds.Size.X * 2.5f) + 2.5f),
+                        (int)(Cursor.Instance.screenPosition.Y + (Cursor.sprite.Bounds.Size.Y * 4f))
+                        ),
+                    new Point(
+                        offset,
+                        fireCooldownRect.Height
+                    ))
+                    , Color.White);
+                //Main.spriteBatch.DrawString(font, ammoText, (Main.screenSize.Size - ammoTextPositionOffset).ToVector2(), ammoAmount >= Player.Instance.equippedWeapon.ammoUsage ? Color.White : errorColor);
             }
 
             Main.spriteBatch.Draw(healthbarSprite, new Rectangle(
@@ -79,10 +104,14 @@ namespace ConcentratedHell.UI
             Cursor.Instance.Draw();
             PickupText.StaticDraw();
 
-            string debugText = $"{fps}\n\nEntities : {Entity.Entity.collection.Count}\nProjectiles : {Combat.Projectiles.Projectile.collection.Count}\nParticles : {Particles.Particle.particles.Count}";
+            string debugText = $"" +
+                $"{fps}\n\n" +
+                $"Entities : {Entity.Entity.collection.Count}\n" +
+                $"Projectiles : {Combat.Projectiles.Projectile.collection.Count}\n" +
+                $"Particles : {Particles.Particle.particles.Count}\n";
             if (showDebug)
             {
-                Main.spriteBatch.DrawString(UI.font, debugText, Vector2.Zero, Color.White);
+                Main.spriteBatch.DrawString(font, debugText, Vector2.Zero, Color.White);
             }
         }
     }
